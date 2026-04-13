07a122a509d4437b83c56a5eb388768fa07678ac
5460-ocean <magmuchai15@gmail.com>
2026-04-13 17:15:19 +0000

    Reset DB for role column

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
-from app.routers import auth, project, admin, logs, analytics
+from app.database import Base, engine
+
+# 🔥 FORCE recreate tables
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+from app.routers import auth, project, admin, logs, analytics
 
 app.include_router(auth.router)
 app.include_router(project.router)
 app.include_router(admin.router)
 app.include_router(logs.router)
 app.include_router(analytics.router)
+
 
 @app.get("/")
 def root():
