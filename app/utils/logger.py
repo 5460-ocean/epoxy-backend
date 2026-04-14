892b9714966d6f3ab9f05c70e2b9acd613f466c3
5460-ocean <magmuchai15@gmail.com>
2026-04-14 06:56:39 +0000

    Debug logger

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,6 +1,8 @@
 from app import models
 
 def create_log(db, user_id: int, action: str):
+    print("🔥 LOGGING:", action)   # DEBUG
+
     log = models.Log(
         user_id=user_id,
         action=action
