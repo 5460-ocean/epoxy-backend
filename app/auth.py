ea1fad60f6510fe14e6fd0fea4634c2b4c5e3f2d
5460-ocean <magmuchai15@gmail.com>
2026-04-23 14:38:31 +0000

    fix auth router

--- app/auth.py
+++ app/auth.py
@@ -1,25 +1,11 @@
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from jose import jwt
+from fastapi import APIRouter
 
-SECRET_KEY = "super-secret-key"  # 🔥 change later in production
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+@router.post("/register")
+def register():
+    return {"message": "register working"}
 
-
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+@router.post("/login")
+def login():
+    return {"message": "login working"}
