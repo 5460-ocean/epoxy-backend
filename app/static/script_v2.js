15e251fa037deca62b02fffb72dd900121b46a97
5460-ocean <magmuchai15@gmail.com>
2026-04-30 22:34:55 +0000

    fix correct path shader

--- app/static/script_v2.js
+++ app/static/script_v2.js
@@ -1,1 +1,68 @@
-alert("APP STATIC FILE LOADED");
+alert("REAL SHADER LOADED");
+
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl");
+
+canvas.width = window.innerWidth;
+canvas.height = 300;
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+const vertexShaderSource = `
+attribute vec2 position;
+void main(){
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+`;
+
+const fragmentShaderSource = `
+precision mediump float;
+
+void main(){
+    vec2 uv = gl_FragCoord.xy / vec2(800.0,300.0);
+    gl_FragColor = vec4(uv.x, uv.y, 0.5, 1.0);
+}
+`;
+
+function createShader(gl, type, src){
+    const s = gl.createShader(type);
+    gl.shaderSource(s, src);
+    gl.compileShader(s);
+
+    if(!gl.getShaderParameter(s, gl.COMPILE_STATUS)){
+        alert(gl.getShaderInfoLog(s));
+    }
+
+    return s;
+}
+
+const vs = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fs = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+const program = gl.createProgram();
+gl.attachShader(program, vs);
+gl.attachShader(program, fs);
+gl.linkProgram(program);
+
+if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+    alert(gl.getProgramInfoLog(program));
+}
+
+gl.useProgram(program);
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+-1,-1, 1,-1, -1,1,
+-1,1, 1,-1, 1,1
+]), gl.STATIC_DRAW);
+
+const pos = gl.getAttribLocation(program,"position");
+gl.enableVertexAttribArray(pos);
+gl.vertexAttribPointer(pos,2,gl.FLOAT,false,0,0);
+
+function render(){
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    requestAnimationFrame(render);
+}
+render();
