791d82c8655f9f64443ce229e925a0e678c74756
5460-ocean <magmuchai15@gmail.com>
2026-05-05 17:45:40 +0000

    force test route

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from app.main import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message": "API running"}
+
+@app.route("/app")
+def app_page():
+    return "THIS IS A TEST PAGE"
+
+if __name__ == "__main__":
+    app.run()
