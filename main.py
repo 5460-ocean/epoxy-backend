793341bb6c3c40c4fa8a6ec8b36a4fa8a9564f9d
5460-ocean <magmuchai15@gmail.com>
2026-05-08 08:03:45 +0000

    Restore clean v5 route

--- main.py
+++ main.py
@@ -46,3 +46,39 @@ def app_page():
 
 
 
+
+
+@app.get("/app/v5")
+async def app_v5():
+    return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Epoxy Shader V5</title>
+
+<style>
+html, body {
+    margin: 0;
+    overflow: hidden;
+    background: black;
+}
+
+canvas {
+    width: 100vw;
+    height: 100vh;
+    display: block;
+}
+</style>
+</head>
+
+<body>
+
+<canvas id="glcanvas"></canvas>
+
+<script src="/static/script_v5.js"></script>
+
+</body>
+</html>
+"""
